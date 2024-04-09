0044a4f6a56ed7c21de5ea908fc2abecfea07219
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-04-09 20:48:45 +0200

    update

--- app.js
+++ app.js
@@ -1,43 +1,27 @@
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
 
-
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-import contactsRouter from "./routes/contactsRouter.js";
-
-dotenv.config();
+const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
 
-const { DB_HOST, PORT = 3000 } = process.env;
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-mongoose.set("strictQuery", true);
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    app.listen(PORT);
-    console.log("Database connection successful.");
-    console.log(`port: ${PORT}`);
-  })
-  .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
-  });
-
-app.use(morgan("tiny"));
+app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+app.use((req, res) => {
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
+
+module.exports = app;
