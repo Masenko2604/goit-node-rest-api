c4450d14b849b8d656d56eb477423d5063e1ab81
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-04-19 14:26:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import 'dotenv/config';
 import { app } from "./app.js";
 
-const { DB_HOST, PORT = 3001 } = process.env;
+const { DB_HOST, PORT = 3000 } = process.env;
 
 mongoose.connect(DB_HOST)
     .then(() => {
         app.listen(PORT, () => {
-          console.log('Server is running on port 3001');
+          console.log('Server is running on port 3000');
         });
        
     })
