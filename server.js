1bc119a9de7cb9ddd4af0a77ca9d7cb9f65115de
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-04-19 14:25:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,10 @@ const { DB_HOST, PORT = 3001 } = process.env;
 
 mongoose.connect(DB_HOST)
     .then(() => {
-        app.listen(PORT);
-        console.log('Server is running on port 3001');
+        app.listen(PORT, () => {
+          console.log('Server is running on port 3001');
+        });
+       
     })
     .catch((err) => {
         console.log(err.message);
