b8eb6f696986315b8e6be389661c6c502481be5b
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-04-19 21:40:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,27 @@ import mongoose from "mongoose";
 import 'dotenv/config';
 import { app } from "./app.js";
 
-const { DB_HOST, PORT = 3000 } = process.env;
+// const { DB_HOST, PORT = 3000 } = process.env;
 
-mongoose.connect(DB_HOST)
-    .then(() => {
-        app.listen(PORT, () => {
-          console.log('Server is running on port 3000');
-        });
+// mongoose.connect(DB_HOST)
+//     .then(() => {
+//         app.listen(PORT, () => {
+//           console.log('Server is running on port 3000');
+//         });
        
-    })
-    .catch((err) => {
-        console.log(err.message);
-        process.exit(1);
+//     })
+//     .catch((err) => {
+//         console.log(err.message);
+//         process.exit(1);
 
-    })
+//     })
+const express = require("express");
+const app = express();
+const port = 3001;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+userSchema.post("save", HandleMongooseError);
+const User = model("user", userSchema);
