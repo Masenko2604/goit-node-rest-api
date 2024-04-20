61e5e9c0a1fa4ae6a1ead0fc88d57d8ac7399f49
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-04-20 13:58:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,28 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import 'dotenv/config';
+import {contactsRouter} from "./routes/contactsRouter.js";
+import  {authRouter}  from './routes/api/auth.js';
+// import { NewAvatar } from "./temp/changeImg.js";
 
-import contactsRouter from "./routes/contactsRouter.js";
-import mongoose from "mongoose";
-import "dotenv/config";
-import authRouter from "./routes/userRouter.js"
 
-const app = express();
+export const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use("/api/users", authRouter)
+app.use("/api/users", authRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
-    res.status(404).json({ message: "Route not found" });
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-    const { status = 500, message = "Server error" } = err;
-    res.status(status).json({ message });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
 });
-
-const { MONGODB_URL, PORT } = process.env;
-
-mongoose
-    .connect(MONGODB_URL)
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Database connection successful`);
-        });
-    })
-    .catch((err) => {
-        console.log(err.message);
-        process.exit(1);
-    });
+// NewAvatar;
