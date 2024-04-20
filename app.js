c5b099e16023375c333b37fd5aacb44806ef4dd3
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-04-20 13:47:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,40 @@
-import express, { json } from "express";
+import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+
 import contactsRouter from "./routes/contactsRouter.js";
-import dotenv from "dotenv";
-import { errorHandler } from "./controllers/errorController.js";
 import mongoose from "mongoose";
-import usersRouter from "./routes/usersRouter.js";
-
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("Database connection successful");
-  })
-  .catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+import "dotenv/config";
+import authRouter from "./routes/userRouter.js"
 
 const app = express();
 
-if ((process.env.NODE_ENV = "development")) {
-  app.use(morgan("tiny"));
-}
-
+app.use(morgan("tiny"));
 app.use(cors());
-
 app.use(express.json());
 
-app.use(express.static("tmp"));
-
+app.use("/api/users", authRouter)
 app.use("/api/contacts", contactsRouter);
 
-app.use("/users", usersRouter);
-
-
-
-app.all("*", (req, res) => {
-  res.status(404).json({ message: "Resource not found" });
+app.use((_, res) => {
+    res.status(404).json({ message: "Route not found" });
 });
 
-app.use(errorHandler);
-
-const port = process.env.PORT ? +process.env.PORT : 3000;
-app.listen(port, () => {
-  console.log("Server is running. Use our API on port: 3000");
+app.use((err, req, res, next) => {
+    const { status = 500, message = "Server error" } = err;
+    res.status(status).json({ message });
 });
+
+const { MONGODB_URL, PORT } = process.env;
+
+mongoose
+    .connect(MONGODB_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Database connection successful`);
+        });
+    })
+    .catch((err) => {
+        console.log(err.message);
+        process.exit(1);
+    });
