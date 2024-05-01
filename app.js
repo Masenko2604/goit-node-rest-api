d9751db118b5aec97978d4055e2b84ed7b62671e
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-05-01 21:02:05 +0200

    p.json

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import 'dotenv/config';
-import {contactsRouter} from "./routes/contactsRouter.js";
-import  {authRouter}  from './routes/api/auth.js';
-// import { NewAvatar } from "./temp/changeImg.js";
+import contactsRouter from "./routes/contactsRouter.js";
+import usersRouter from "./routes/usersRouter.js";
 
-
-export const app = express();
+const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("public"))
 
-app.use("/api/users", authRouter);
+app.use("/api/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
@@ -22,6 +19,15 @@ app.use((_, res) => {
 });
 
 app.use((err, req, res, next) => {
+
+  const { code, name } = err;
+  if (code === 11000 && name === "MongoServerError") {
+    res.status(409).json({ message: "Email in use" });
+    return;
+  }
+
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
+
+export default app
