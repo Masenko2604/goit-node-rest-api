582c0f5585df93f531610cdc3cb9df7e8d28e7af
Masenko2604 <138580178+Masenko2604@users.noreply.github.com>
2024-05-02 13:52:10 +0200

    update

--- middlewars/index.js
+++ middlewars/index.js
@@ -2,6 +2,6 @@ import authenticate from "./authenticate.js";
 import isValidId from "./isValidId.js";
 import validateBody from "./validateBody.js";
 import upload from "./upload.js";
-import index from "./index.js";
 
-export { authenticate, index, isValidId, validateBody, upload };
+
+export { authenticate, isValidId, validateBody, upload };
